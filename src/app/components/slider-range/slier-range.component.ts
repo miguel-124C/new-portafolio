419087d7ac4c8a5b048877a1c19b6c79d6a7cb92
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2023-10-10 11:05:34 -0400

    Se hace responsive pantalla de skills

--- src/app/components/slider-range/slier-range.component.ts
+++ src/app/components/slider-range/slier-range.component.ts
@@ -1,14 +1,43 @@
-import { Component, Input } from '@angular/core';
-import { Skill } from 'src/app/interfaces/skill.interface';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'c-slider-range',
   templateUrl: './slier-range.component.html',
   styleUrls: ['./slier-range.component.scss']
 })
-export class SlierRangeComponent {
+export class SlierRangeComponent implements OnInit {
 
   @Input()
-  public skills!: Skill[];
+  public percent: number = 0;
+
+  @ViewChild('percent')
+  public percentElement!: ElementRef<HTMLDivElement>;
+
+  public keyFrames: Keyframe[] = [
+    { width: '0%', offset: 0 }
+  ];
+
+  public options: KeyframeAnimationOptions = {
+    duration: 600,
+    direction: 'normal',
+    fill: 'forwards',
+  }
+
+  ngOnInit(): void {
+    this.showPercent();
+  }
+
+  constructor(){}
+
+  showPercent(){
+    this.keyFrames = [
+      { width: '0%', offset: 0 },
+      { width: `${this.percent}%` }
+    ];
+
+    setTimeout(()=>{
+      this.percentElement.nativeElement.animate(this.keyFrames, this.options);
+    }, 10);
+  }
 
 }
