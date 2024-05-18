ec3298940efe780aab1b26f02c74cfca0a1d571d
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2024-05-17 21:20:51 -0400

    Solucion de error de importaciones

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home-page/home.component';
+import { ComponentsModule } from '../shared/components/components.module';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    AboutMeComponent,
+    PortafolioComponent,
+    SkillsComponent,
   ],
   imports: [
-    
+    ComponentsModule
   ],
   exports: [
     HomeComponent,
