51310787cd9a43a06eecbc7db3ed9c28ebbe12a3
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2024-05-25 15:16:22 -0400

    organizacion de imagenes

--- db/db-proyects.ts
+++ db/db-proyects.ts
@@ -1,97 +1,75 @@
 import { Wokrs } from "src/app/interfaces/works.interface";
 
+const pathImages = 'assets/img/proyects/';
+
 export const Proyects: Wokrs[] = [
   {
-    srcImg:'assets/img/1.png',
+    srcImg:`${pathImages}app-alarma-js.png`,
     name:'RelojApp',
     hrefDemo:'https://miguel-124c.github.io/Cronometro-Alarma/',
     hrefCode: ''
   },{
-    srcImg:'assets/img/2.png',
-    
+    srcImg:`${pathImages}relojRgb.png`,
     name:'RelojRgb',
     hrefDemo:'https://miguel-124c.github.io/RelojRgb/',
     hrefCode: ''
   },{
-    srcImg:'assets/img/3.png',      
-    
+    srcImg:`${pathImages}ritmo.png`,
     name:'Ritmo',
     hrefDemo:'https://miguel-124c.github.io/Memoriza-la-cancion/',
     hrefCode: ''
   },{
-    srcImg:'assets/img/4.png', 
+    srcImg:`${pathImages}sudoku.png`,
     name:'Sudoku',
     hrefDemo:'https://miguel-124c.github.io/Sudoku/',
     hrefCode: ''
   },{
-    srcImg:'assets/img/5.png',      
-    
+    srcImg:`${pathImages}pelis.png`,
     name:'Pelis',
     hrefDemo:'https://miguel-124c.github.io/Info-Movies/',
     hrefCode: ''
   },{
-    srcImg:'assets/img/6.png',
-    name:'ListTask',
-    hrefDemo:'https://miguel-124c.github.io/CRUD-Lista-de-tareas/',
-    hrefCode: ''
-  },{
-    srcImg:'assets/img/7.png',      
-    
-    name:'Calendario',
-    hrefDemo:'https://miguel-124c.github.io/Calendario-Object-Date-/',
-    hrefCode: ''
-  },{
-    srcImg:'assets/img/8.png',      
-    
+    srcImg:`${pathImages}encriptador.png`,
     name:'Encriptador',
     hrefDemo:'https://miguel-124c.github.io/Encriptador-Alura/',
     hrefCode: ''
   },{
-    srcImg:'assets/img/9.png',
-    
+    srcImg:`${pathImages}clima.png`,
     name:'Clima',
     hrefDemo:'https://miguel-124c.github.io/datosMeteorologicas/',
     hrefCode: ''
   },{
-    srcImg: 'assets/img/10.png',
-    
+    srcImg:`${pathImages}rick-morty.png`,
     name: 'Rick & Morty',
     hrefDemo: 'https://miguel-124c.github.io/ProyectoCursoDeSisco/',
     hrefCode: ''
   },{
-    srcImg: 'assets/img/11.png',
-    
+    srcImg:`${pathImages}numero.png`,
     name: 'Numero',
     hrefDemo: 'https://miguel-124c.github.io/Random-number/',
     hrefCode: ''
   },{
-    srcImg: 'assets/img/12.png',
-    
-    name: 'Calculadora',
-    hrefDemo: 'https://miguel-124c.github.io/Calculadora/',
-    hrefCode: ''
-  },{
-    srcImg:'assets/img/gifts.png',
+    srcImg:`${pathImages}gifts.png`,
     name:'Gifts App',
     hrefDemo:'https://giftsa44.netlify.app',
     hrefCode: ''
   },{
-    srcImg:'assets/img/country.png',
+    srcImg:`${pathImages}country.png`,
     name:'Country App',
     hrefDemo:'https://countries-app4.netlify.app/countries/by-capital',
     hrefCode: ''
   },{
-    srcImg:'assets/img/conversores.png',
+    srcImg:`${pathImages}conversores.png`,
     name:'Conversores',
     hrefDemo:'https://coversores.netlify.app/conversor/monedas',
     hrefCode: ''
   },{
-    srcImg:'assets/img/maps.png',
+    srcImg:`${pathImages}maps.png`,
     name:'Maps App',
     hrefDemo:'https://mapsapplication.netlify.app/maps/fullScreen',
     hrefCode: ''
   },{
-    srcImg: 'assets/img/13.png',
+    srcImg:`${pathImages}app-reloj-angular.png`,
     name: 'AppReloj',
     hrefDemo: 'https://app-reloj.netlify.app/alarma',
     hrefCode: ''
