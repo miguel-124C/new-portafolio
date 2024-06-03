5d93cd78ba572bd46345b16dabdea96200ba0807
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2024-06-03 12:29:23 -0400

    Se termina seccion de contactame

--- src/app/layouts/contac-me-section/contac-me-section.component.ts
+++ src/app/layouts/contac-me-section/contac-me-section.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { GMAIL, PHONE, UrlPersonals } from 'db/dataPersonal';
+import { Redes } from 'src/app/interfaces/redes.interface';
 
 @Component({
   selector: 'app-contac-me-section',
   templateUrl: './contac-me-section.component.html',
   styleUrls: ['./contac-me-section.component.scss'],
 })
-export class ContacMeSectionComponent {
+export class ContacMeSectionComponent implements AfterViewInit {
 
+  public urlPersonals = {};
+  public redes: Redes[] = [];
 
+  public constructor(){
+    this.urlPersonals = UrlPersonals;
+
+  }
+
+  ngAfterViewInit(): void {
+    this.redes = [
+      {
+        href: UrlPersonals.whatsApp,
+        icon: 'fa-brands fa-whatsapp fa-2xl',
+        title: `+${PHONE}`
+      },{
+        href: UrlPersonals.gmail,
+        icon: 'fa-solid fa-envelope fa-2xl',
+        title: GMAIL,
+      },{
+        href: UrlPersonals.linkEdin,
+        icon: 'fa-brands fa-linkedin fa-2xl',
+        title: 'LinkEdin'
+      },{
+        href: `tel:${PHONE}`,
+        icon: 'fa-solid fa-phone fa-2xl',
+        title: `+${PHONE}`
+      },{
+        href: '',
+        icon: 'fa-solid fa-location-dot fa-2xl',
+        title: 'Santa Cruz - Bolivia'
+      },
+    ]
+  }
 
 }
