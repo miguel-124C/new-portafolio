f43e1b2bc2a561b4c7ba39e22655929fc1ac338a
miguel-124 <mikicuellar20@gmail.com>
2025-05-13 20:05:26 -0400

    Se comentan proyctos y se añaden nuevos

--- db/db-proyects.ts
+++ db/db-proyects.ts
@@ -1,6 +1,6 @@
 import { Wokrs } from "src/app/interfaces/works.interface";
 import { UrlPersonals, NAMEGITHUB } from "./dataPersonal";
-import { _angular, _bootstrap, _css, _express, _html, _js, _nest, _node, _sass, _ts } from "./styleTecnologys";
+import { _angular, _bootstrap, _css, _express, _html, _java, _js, _nest, _node, _sass, _ts } from "./styleTecnologys";
 
 const pathImages = 'assets/img/proyects';
 const pathGitHubPage = `https://${NAMEGITHUB}.github.io`;
@@ -12,18 +12,20 @@ export const Proyects: Wokrs[] = [
     name:'WhatsApp',
     hrefDemo:`https://youtube.com/shorts/CuBcWPlwQlw?feature=share`,
     hrefCode: `${pathGithubCode}/whatsApp-Chat-Front`,
-    useTecnology: [ _html, _css, _js, _express ],
-    category: 'Otros',
-  },{
-    srcImg:`${pathImages}/app-alarma-js.png`,
-    name:'RelojApp',
-    hrefDemo:`${pathGitHubPage}/Cronometro-Alarma`,
-    hrefCode: `${pathGithubCode}/Cronometro-Alarma`,
-    useTecnology: [ _html, _css, _js, ],
-    category: 'Crud',
-  },{
+    useTecnology: [ _html, _css, _js, _express, _ts ],
+    category: 'Apis',
+  }
+  // ,{
+  //   srcImg:`${pathImages}/app-alarma-js.png`,
+  //   name:'RelojApp',
+  //   hrefDemo:`${pathGitHubPage}/Cronometro-Alarma`,
+  //   hrefCode: `${pathGithubCode}/Cronometro-Alarma`,
+  //   useTecnology: [ _html, _css, _js, ],
+  //   category: 'Crud',
+  // }
+  ,{
     srcImg:`${pathImages}/relojRgb.png`,
-    name:'RelojRgb',
+    name:'Reloj - Rgb',
     hrefDemo:`${pathGitHubPage}/RelojRgb`,
     hrefCode: `${pathGithubCode}/RelojRgb`,
     useTecnology: [ _html, _css, _js, ],
@@ -43,6 +45,20 @@ export const Proyects: Wokrs[] = [
     useTecnology: [ _html, _css, _js, ],
     category: 'Game'
   },{
+    srcImg:`${pathImages}/app-reloj-angular.png`,
+    name: 'AppReloj',
+    hrefDemo: 'https://app-reloj.netlify.app/reloj',
+    hrefCode: `${pathGithubCode}/AppReloj-Front-End`,
+    useTecnology: [ _angular, _ts, _sass, _nest ],
+    category: 'Crud'
+  },{
+    srcImg:`${pathImages}/cieloscopio.webp`,
+    name:'Cieloscopio',
+    hrefDemo:`https://youtu.be/X-qBLEfuKc8`,
+    hrefCode: `${pathGithubCode}/Cieloscopio`,
+    useTecnology: [ _java ],
+    category: 'Consola'
+  },{
     srcImg:`${pathImages}/pelis.png`,
     name:'Pelis',
     hrefDemo:`${pathGitHubPage}/Info-Movies`,
@@ -55,14 +71,7 @@ export const Proyects: Wokrs[] = [
     hrefDemo:`${pathGitHubPage}/Encriptador-Alura`,
     hrefCode: `${pathGithubCode}/Encriptador-Alura`,
     useTecnology: [ _html, _css, _js, ],
-    category: 'Cursos'
-  },{
-    srcImg:`${pathImages}/clima.png`,
-    name:'Clima',
-    hrefDemo:`${pathGitHubPage}/datosMeteorologicas`,
-    hrefCode: `${pathGithubCode}/datosMeteorologicas`,
-    useTecnology: [ _html, _css, _js, ],
-    category: 'Apis'
+    category: 'Otros'
   },{
     srcImg:`${pathImages}/rick-morty.png`,
     name: 'Rick & Morty',
@@ -93,27 +102,21 @@ export const Proyects: Wokrs[] = [
   //   useTecnology: [ _angular, _ts, _bootstrap ],
   //   category: 'Cursos'
   // },
+  // {
+  //   srcImg:`${pathImages}/conversores.png`,
+  //   name:'Conversores',
+  //   hrefDemo:'https://coversores.netlify.app/conversor/monedas',
+  //   hrefCode: `${pathGithubCode}/ConversorTs`,
+  //   useTecnology: [ _angular, _ts, _sass ],
+  //   category: 'Otros'
+  // },
   {
-    srcImg:`${pathImages}/conversores.png`,
-    name:'Conversores',
-    hrefDemo:'https://coversores.netlify.app/conversor/monedas',
-    hrefCode: `${pathGithubCode}/ConversorTs`,
-    useTecnology: [ _angular, _ts, _sass ],
-    category: 'Otros'
-  },{
     srcImg:`${pathImages}/maps.png`,
     name:'Maps App',
     hrefDemo:'https://mapsapplication.netlify.app/maps/fullScreen',
     hrefCode: `${pathGithubCode}/09-mapsApp`,
     useTecnology: [ _angular, _ts, _bootstrap ],
-    category: 'Cursos'
-  },{
-    srcImg:`${pathImages}/app-reloj-angular.png`,
-    name: 'AppReloj',
-    hrefDemo: 'https://app-reloj.netlify.app/alarma',
-    hrefCode: `${pathGithubCode}/AppReloj-Front-End`,
-    useTecnology: [ _angular, _ts, _sass, _nest ],
-    category: 'Crud'
+    category: 'Otros'
   },{
     srcImg:`${pathImages}/gato.png`,
     name: 'Gato 3 en raya',
