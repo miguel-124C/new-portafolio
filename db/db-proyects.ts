945c2fe46993f2af43ae9c3710b9a9ef804e3d21
miguel-124 <mikicuellar20@gmail.com>
2026-05-31 18:31:16 -0400

    Adding tetris and flappybird proyect

--- db/db-proyects.ts
+++ db/db-proyects.ts
@@ -1,36 +1,22 @@
-import { Wokrs } from "src/app/interfaces/works.interface";
+import { Works } from "src/app/interfaces/works.interface";
 import { UrlPersonals, NAMEGITHUB } from "./dataPersonal";
-import { _angular, _bootstrap, _css, _docker, _express, _html, _java, _js, _nest, _node, _postgres, _python, _sass, _tailwind, _ts } from "./styleTecnologys";
+import { _angular, _bootstrap, _css, _docker, _express, _html, _java, _js,
+  _nest, _node, _postgres, _python, _sass, _tailwind, _ts, _dotnet } from "./styleTecnologys";
 
 const pathImages = 'assets/img/proyects';
 const pathGitHubPage = `https://${NAMEGITHUB}.github.io`;
 const pathGithubCode = UrlPersonals.gitHub;
+const itch = UrlPersonals.itch;
 
-export const Proyects: Wokrs[] = [
+export const Proyects: Works[] = [
   {
-    srcImg:`${pathImages}/kevin-bacon-game.png`,
-    name:'Kevin Bacon Game',
-    hrefDemo:`https://youtu.be/ZmjoaaNWn6Y?si=iCyVOgyTnjidnLCb`,
-    hrefCode: `${pathGithubCode}/front-six-degrees-separation`,
-    useTecnology: [ _angular, _tailwind, _python ],
-    category: 'Game',
-  },{
     srcImg:`${pathImages}/whatsapp-cloud.png`,
     name:'WhatsApp',
     hrefDemo:`https://youtube.com/shorts/CuBcWPlwQlw?feature=share`,
     hrefCode: `${pathGithubCode}/whatsApp-Chat-Front`,
     useTecnology: [ _html, _css, _js, _express, _ts ],
     category: 'Apis',
-  }
-  // ,{
-  //   srcImg:`${pathImages}/app-alarma-js.png`,
-  //   name:'RelojApp',
-  //   hrefDemo:`${pathGitHubPage}/Cronometro-Alarma`,
-  //   hrefCode: `${pathGithubCode}/Cronometro-Alarma`,
-  //   useTecnology: [ _html, _css, _js, ],
-  //   category: 'Crud',
-  // }
-  ,{
+  },{
     srcImg:`${pathImages}/relojRgb.png`,
     name:'Reloj - Rgb',
     hrefDemo:`${pathGitHubPage}/RelojRgb`,
@@ -38,18 +24,18 @@ export const Proyects: Wokrs[] = [
     useTecnology: [ _html, _css, _js, ],
     category: 'Style'
   },{
+    srcImg:`${pathImages}/cieloscopio.webp`,
+    name:'Cieloscopio',
+    hrefDemo:`https://youtu.be/X-qBLEfuKc8`,
+    hrefCode: `${pathGithubCode}/Cieloscopio`,
+    useTecnology: [ _java ],
+    category: 'Consola'
+  },{
     srcImg:`${pathImages}/gato.png`,
     name: '3 en raya',
     hrefDemo:`${pathGitHubPage}/Gato-3EnRaya`,
     hrefCode: `${pathGithubCode}/Gato-3EnRaya`,
     useTecnology: [ _js, _css, _html ],
-    category: 'Game'
-  },{
-    srcImg:`${pathImages}/ritmo.png`,
-    name:'Ritmo',
-    hrefDemo:`${pathGitHubPage}/Memoriza-la-cancion`,
-    hrefCode: `${pathGithubCode}/Memoriza-la-cancion`,
-    useTecnology: [ _html, _css, _js, ],
     category: 'Game'
   },{
     srcImg:`${pathImages}/sudoku.png`,
@@ -59,26 +45,19 @@ export const Proyects: Wokrs[] = [
     useTecnology: [ _html, _css, _js, ],
     category: 'Game'
   },{
-    srcImg:`${pathImages}/app-reloj-angular.png`,
-    name: 'AppReloj',
-    hrefDemo: 'https://app-reloj.netlify.app/reloj',
-    hrefCode: `${pathGithubCode}/AppReloj-Front-End`,
-    useTecnology: [ _angular, _sass, _nest, _postgres ],
-    category: 'Crud'
+    srcImg: `${pathImages}/flappyBird.png`,
+    name: 'FlappyBird',
+    hrefDemo: `${itch}/flappybird`,
+    hrefCode: `${pathGithubCode}/FlappyBird`,
+    useTecnology: [_java],
+    category: 'Game'
   },{
-    srcImg:`${pathImages}/cieloscopio.webp`,
-    name:'Cieloscopio',
-    hrefDemo:`https://youtu.be/X-qBLEfuKc8`,
-    hrefCode: `${pathGithubCode}/Cieloscopio`,
-    useTecnology: [ _java ],
-    category: 'Consola'
-  },{
-    srcImg:`${pathImages}/pelis.png`,
-    name:'Pelis',
-    hrefDemo:`${pathGitHubPage}/Info-Movies`,
-    hrefCode: `${pathGithubCode}/Info-Movies`,
-    useTecnology: [ _html, _css, _js, ],
-    category: 'Apis'
+    srcImg: `${pathImages}/tetris.png`,
+    name: 'Tetris',
+    hrefDemo: `${itch}/tetris-monogame-edition`,
+    hrefCode: `${pathGithubCode}/TetrisMonoGame`,
+    useTecnology: [_java],
+    category: 'Game'
   },{
     srcImg:`${pathImages}/encriptador.png`,
     name:'Encriptador',
@@ -93,44 +72,34 @@ export const Proyects: Wokrs[] = [
     hrefCode: `${pathGithubCode}/ProyectoCursoDeSisco`,
     useTecnology: [ _html, _css, _js, ],
     category: 'Apis'
-  },
-  // {
-  //   srcImg:`${pathImages}/numero.png`,
-  //   name: 'Numero',
-  //   hrefDemo:`${pathGitHubPage}/Random-number`,
-  //   hrefCode: `${pathGithubCode}/Random-number`,
-  //   useTecnology: [ _html, _css, _js, ],
-  //   category: 'Game'
-  // },{
-  //   srcImg:`${pathImages}/gifts.png`,
-  //   name:'Gifts App',
-  //   hrefDemo:'https://giftsa44.netlify.app',
-  //   hrefCode: `${pathGithubCode}/gifs-app`,
-  //   useTecnology: [ _angular, _ts, _bootstrap ],
-  //   category: 'Cursos'
-  // },{
-  //   srcImg:`${pathImages}/country.png`,
-  //   name:'Country App',
-  //   hrefDemo:'https://countries-app4.netlify.app/countries/by-capital',
-  //   hrefCode: `${pathGithubCode}/countryApp`,
-  //   useTecnology: [ _angular, _ts, _bootstrap ],
-  //   category: 'Cursos'
-  // },
-  // {
-  //   srcImg:`${pathImages}/conversores.png`,
-  //   name:'Conversores',
-  //   hrefDemo:'https://coversores.netlify.app/conversor/monedas',
-  //   hrefCode: `${pathGithubCode}/ConversorTs`,
-  //   useTecnology: [ _angular, _ts, _sass ],
-  //   category: 'Otros'
-  // },
-  {
+  },{
+    srcImg:`${pathImages}/ritmo.png`,
+    name:'Ritmo',
+    hrefDemo:`${pathGitHubPage}/Memoriza-la-cancion`,
+    hrefCode: `${pathGithubCode}/Memoriza-la-cancion`,
+    useTecnology: [ _html, _css, _js, ],
+    category: 'Game'
+  },{
     srcImg:`${pathImages}/maps.png`,
     name:'Maps App',
     hrefDemo:'https://mapsapplication.netlify.app/maps/fullScreen',
     hrefCode: `${pathGithubCode}/09-mapsApp`,
     useTecnology: [ _angular, _ts, _bootstrap ],
     category: 'Otros'
+  },{
+    srcImg:`${pathImages}/kevin-bacon-game.png`,
+    name:'KevinBaconGame',
+    hrefDemo:`https://youtu.be/ZmjoaaNWn6Y?si=iCyVOgyTnjidnLCb`,
+    hrefCode: `${pathGithubCode}/front-six-degrees-separation`,
+    useTecnology: [ _angular, _tailwind, _python ],
+    category: 'Game',
+  },{
+    srcImg:`${pathImages}/pelis.png`,
+    name:'Pelis',
+    hrefDemo:`${pathGitHubPage}/Info-Movies`,
+    hrefCode: `${pathGithubCode}/Info-Movies`,
+    useTecnology: [ _html, _css, _js, ],
+    category: 'Apis'
   },{
     srcImg:``,
     name:'StoreFree',
@@ -139,13 +108,18 @@ export const Proyects: Wokrs[] = [
     useTecnology: [ _angular, _nest, _postgres, _docker ],
     category: 'inProgress',
     description: `Ecommerce de publicaciones para vender y comprar. Con metodo de pago ficticio, se esta desarrollando de apoco y usando muchas herramientas`,
-  },{
-    srcImg:``,
-    name:'Tetris',
-    hrefDemo:'https://www.youtube.com/embed/EvHhVggUMYg',
-    hrefCode: `#`,
-    useTecnology: [ _java ],
-    category: 'inProgress',
-    description: `Clasico juego de Tetris, hecho para Desktop. Proximamente tutorial`,
   }
+  // ,{
+  //   srcImg:``,
+  //   name:'Galaga',
+  //   hrefDemo:'',
+  //   hrefCode: `#`,
+  //   useTecnology: [ _dotnet ],
+  //   category: 'inProgress',
+  //   description: `
+  //     Videojuego retro desarrollandose en C# con el framework Monogame.\n
+  //     Implementando la arquitectura ECS y patrones de diseños tales como:\n
+  //       Patron Observer y el Factory.
+  //   `,
+  // }
 ];
